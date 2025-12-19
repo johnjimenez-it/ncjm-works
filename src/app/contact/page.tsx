0feadb3f9c1112db416b547175cbd81e1a1197e9
john <johnjcla@gmail.com>
2025-12-19 03:26:47 -0500

    Update Google Maps embed to Miami Metropolitan Area
    Replaced the embedded Google Maps iframe on the contact page to display the Miami Metropolitan Area instead of Hallandale Beach, FL.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -401,7 +401,7 @@ export default function ContactPage() {
           <div className="rounded-2xl overflow-hidden shadow-xl border border-slate-200">
             <div className="relative w-full h-96 bg-slate-100">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114841.02685890885!2d-80.207914!3d25.9856275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9ab6aeb010e97%3A0xc069f9e7104b281b!2sHallandale%20Beach%2C%20FL%2033009!5e0!3m2!1sen!2sus!4v1718788934567!5m2!1sen!2sus"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d854274.7053997401!2d-80.43026900000001!3d26.054181550000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9745374484071%3A0xeb174b8d9d82af56!2sMiami%20Metropolitan%20Area%2C%20FL!5e1!3m2!1sen!2sus!4v1766132760276!5m2!1sen!2sus"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
